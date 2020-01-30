9cf8baf404c4cd83d86357b5b96e2a9b35bef293
Alexander <Cuel@users.noreply.github.com>
2020-01-30 15:47:58 +0800

    major restrcture for bottom bar navigation

---
+++ lib/screens/games/containers/games.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:matchpoint/screens/games/components/create-game.dart';
+import 'package:matchpoint/screens/games/components/games-list.dart';
+
+class GamesContainer extends StatefulWidget {
+  const GamesContainer({Key key}) : super(key: key);
+
+  @override
+  _GamesContainerState createState() => _GamesContainerState();
+}
+
+class _GamesContainerState extends State<GamesContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Test"),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              print('press');
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (ctx) => CreateGame(
+                            pageTitle: "test",
+                          ),
+                      fullscreenDialog: true));
+            }),
+        body: GamesList());
+  }
+}
